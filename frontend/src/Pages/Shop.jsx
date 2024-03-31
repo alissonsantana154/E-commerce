833c6e9edb78c8c1f5e11763447173b37c9b3bd4
Alisson Santana <alissonsantana16608@gmail.com>
2024-03-31 17:19:13 -0300

    feat: Component New Collections

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,13 +1,15 @@
 import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
 import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
   return (
     <div>
-      <Hero />
-      <Popular />
-      <Offers />
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
     </div>
   )
 }
